9afcf106d9e5470138d659de0fc7a9404bda074c
Stephan Stock <s.stock@hotmail.de>
2025-02-20 11:10:49 +0100

    Modulabschlussprojekt Endphase hochgeladen

--- scripts/python/MAP/Modulabschlussprojekt_Python_Stephan_Stock.py
+++ scripts/python/MAP/Modulabschlussprojekt_Python_Stephan_Stock.py
@@ -1,5 +1,7 @@
 #### Modulabschlussprojekt Stephan Stock ####
 
+### Klassen bestimmen und für jede Klasse einen Stringoutput bestimmt.
+
 class Schiff:
     def __init__(self, Schiffname,Baujahr,Heimathafen):
         self.Schiffname = Schiffname
@@ -18,20 +20,24 @@ class Fisch:
     def __str__(self):
         return f"Die Fischart {self.Fischart} wurde im Gebiet {self.Fanggebiet}gefangen, und kostet {self.Preis} das Kilo"
 
-
-
 class Fangergebnis:
-    def __init__(self, fangSchiff,fangKapitän,fangDatum,fangFisch,fangFangmenge):
-        self.Schiff = fangSchiff
-        self.Kapitän = fangKapitän
-        self.Datum = fangDatum
-        self.Fisch = fangFisch
-        self.Fangmenge = fangFangmenge
+    def __init__(self, Schiff,Kapitän,Datum,Fisch,Fangmenge):
+        self.Schiff = Schiff
+        self.Kapitän = Kapitän
+        self.Datum = Datum
+        self.Fisch = Fisch
+        self.Fangmenge = Fangmenge
 
     def __str__(self):
-        return f"Das Schiff {self.Schiff} mit dem Kapitän {self.Kapitän} hat am {self.Datum} den {self.Fisch} mit einer Menge von {self.Fangmenge} Tonnen gesammelt."
+        return f"Schiff {self.Schiff.Schiffname} Kapitän: {self.Kapitän} Datum: {self.Datum} Fisch: {self.Fisch.Fischart} Menge: {self.Fangmenge} Tonnen gesammelt."
 
-############ Listen Einlesen
+############ Listen Einlesen mit Definitionen das die Fische und Schiffe von der Liste Fangmenge 
+############ direkt richtig der Klassen zugeordnet werden.
+
+def finde_schiff(schiffname):
+    for schiff in liste_schiffe:
+        if schiff.Schiffname == schiffname:
+            return schiff
 
 liste_schiffe = list()
 with open("schiffe.csv", "r", encoding="latin-1") as csv_schiffe:
@@ -57,15 +63,9 @@ with open("fangergebnisse.csv" , "r" , encoding="latin-1") as csv_fangergebnisse
     next(csv_fangergebnisse)
     for zeile in csv_fangergebnisse:
         Schiffname, Kapitän, Datum, Fischname, Fangmenge, = zeile.strip().split(";")
-        liste_fangmenge.append(Fangergebnis(Schiffname,Kapitän,Datum,finde_fisch(Fischname), int(Fangmenge)))
+        liste_fangmenge.append(Fangergebnis(finde_schiff(Schiffname),Kapitän,Datum,finde_fisch(Fischname), int(Fangmenge)))
 
 ############## Definitionen Definieren
-
-# Fische die eingelesen werden automatisch in die Klassen packen
-
-
-        
-# Platzhalter für finde_schiffe
 
 # Definition bester Tag wo ausgerechnet wird welcher Tag der Fänge am besten war
 
@@ -103,7 +103,9 @@ def bester_kapitaen():
             bcpt = Kapitän
     print(f"\nDer beste Kapitän war:{bcpt}, mit einer Fangmenge von: {bfang} Tonnen.\n")
 
-# Menübereich bearbeitung
+########### Menübereich Bearbeitung
+
+# Hauptmenu was am Anfang des Programms aufgerufen wird und zudem man immer wieder zurück kommt.
 
 def menu():
     print("Hauptmenü")
@@ -124,6 +126,7 @@ def menu():
         case _:
             print("Auswahl Ungültig")
 
+# Untermenü für die zwei abfragen wie wir Bestimmt haben für Bester Tag und Bester Kapitän
 
 def abfragen():
     print("\n### Menü: Abfragen ###")
@@ -145,17 +148,40 @@ def abfragen():
         case _:
             print("Auswahl Ungültig")
 
-def delete_eintrag():
-    print("\n### Menü: Eintrag Entfernen")
+# Untermenü Fangergebnisse wenn wir das Menü betreten wird uns direkt die Liste der Fangmenge und einer Index
+# Zahl ausgegeben mit listebearbeiten kommen wir in das Untermenü zum Listen bearbeiten
 
 def fangergebnisse():
     print("\n### Menu: Fangergebnisse ###")
+    index = 0
+    for x in liste_fangmenge:
+        index +=1
+        print(f"({index}) {x}")
+    print("(1) Liste bearbeiten")
+    print("(2) Zurück")
+    auswahl = input("Bitte gebe deine Auswahl ein: \n")
+    
+    match auswahl:
+        case "1":
+            listebearbeiten()
+        case "2":
+            menu()
+        case _:
+            print("Ungültige Auswahl")
+
+# Untermenü Liste Bearbeiten, wird aufgerufen um Einträge anzulegen (Bonusaufgabe nicht umgesetzt), Einträge zu
+# Löschen und um die Liste zu speichern. Nach erfolgreicher bearbeitung oder fehlerhafter bearbeitung wird wieder
+# in das Menü zurückgekehrt.
+
+def listebearbeiten():
+    print("\n### Menu: Liste Bearbeiten ###")
+
     print("Wähle aus was du machen willst:")
     print("(1) Eintrag anlegen(noch nicht implementiert)")
     print("(2) Eintrag Löschen")
     print("(3) Liste Speichern")
-    print("Hauptmenü")
-    auswahl = input("Bitte gebe deine Auswahl ein:")
+    print("(4) Hauptmenü")
+    auswahl = input("Bitte gebe deine Auswahl ein: \n")
 
     match auswahl:
         case "1":
@@ -163,30 +189,48 @@ def fangergebnisse():
             fangergebnisse()
         case "2":
             delete_eintrag()
+            listebearbeiten()
         case "3":
             save_list()
+            listebearbeiten()
         case "4":
             menu()
         case _:
             print("Ungültige Auswahl")
 
-######## Bereich für Probieren
+# Untermenü Eintrag Löschen, Hier wird angegeben welcher eintrag aus der Liste gelöscht werden soll.
+# Die Liste wird von liste bearbeiten angezeigt und hier lässt sich mittels einer auswahl der Eintrag
+# mittels der Indexnummer löschen.
 
-# for x in liste_schiffe:
-#     print(x)
+def delete_eintrag():
+    print("\n### Menü: Eintrag Entfernen")
+    print("Nummer Angeben welchen eintrag du löschen willst:")
+    auswahl = int(input("Auswahl treffen!\n"))
+        
+    if liste_fangmenge[auswahl]:
+        liste_fangmenge.pop(auswahl)
+        print(f"Eintrag: ({liste_fangmenge[auswahl]}) wurde Entfernt")
+    else:
+        print("Ungültige Auswahl")
+        
+# Untermenü Liste speichern, Wird benutzt um die Fangmengen Liste die wir mit der String representation ausgegeben
+# bekommen in eine TextDatei mit dem Namen "Fangergebnisse.txt" zu speichern.
+# Als prompt bekommen wir die aussage das die Liste im Stammverzeichniss gespeichert ist.
+        
+def save_list():
+    print("\n### Menu : Liste speichern ###")
+    print("(1) Zum Liste speichern")
+    print("(2) Zurück")
+    auswahl = input("Bitte gebe eine Auswahl ein: \n")
 
-# for x in liste_fische:
-#     print(x)
-
-# for x in liste_fangmenge:
-#     print(x)
-
-#bester_tag()
-
-#bester_kapitaen()
-
-# for x in liste_fangmenge:
-#     print(x)#
+    match auswahl:
+        case "1":
+            with open("Fangergebnisse.txt", "w", encoding="latin-1") as txt_file:
+                for x in liste_fangmenge:
+                    txt_file.write(f"{x} \n")
+            print("Liste im Stammverzeichniss gespeichert!\n")
+        case "2":
+            fangergebnisse()
 
 ##### Start des Programmes
 
